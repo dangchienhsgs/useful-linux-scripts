5ae880f304847434fdd6ef40d67ba33fe10f9c5d
Chien Nguyen Dang <dangchienhsgs@gmail.com>
2015-06-21 23:42:07 +0700

    add gradle to nesscessary packages

--- install_nesscesary_packages.sh
+++ install_nesscesary_packages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y gdebi synaptic ibus-unikey python-sklearn git zsh vim vim-gnome vim-gtk qbittorrent python-pip bpython mc 
+sudo apt-get install -y gdebi synaptic ibus-unikey python-sklearn git zsh vim vim-gnome vim-gtk qbittorrent python-pip bpython mc gradle openjdk-8-jdk npm ruby nodejs nodejs-legacy node
